fe26aab16d719628882a0e422d80a4b734acd39d
richbachman <rb@richbachman.com>
2022-08-29 14:52:22 -0500

    test: add jest config and gh action

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,34 @@
 module.exports = {
-  testMatch: ["**/+(*.)+(spec|test).+(ts|js)?(x)"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  testEnvironment: "jsdom",
+  verbose: true,
+  roots: ["<rootDir>/packages"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  setupFilesAfterEnv: ["../../jest-setup.ts"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testPathIgnorePatterns: ["<rootDir>/packages/(?:.+?)/dist/"],
+  cacheDirectory: ".jest-cache",
+  coverageDirectory: ".jest-coverage",
+  coveragePathIgnorePatterns: ["<rootDir>/packages/(?:.+?)/dist/"],
+  coverageReporters: ["html", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  moduleNameMapper: {
+    ["\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$"]:
+      "<rootDir>/.jest/file-mock.ts",
+  },
+  setupFilesAfterEnv: ["<rootDir>/.jest/jest-setup.ts"],
+  transformIgnorePatterns: ["node_modules/"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
 };
