83ac67a70c7b57de96b6dc49225b7d5f32329833
richbachman <rb@richbachman.com>
2022-09-20 16:36:06 -0500

    fix(components): set as prop as optional

--- packages/components/src/primitives/Box/BaseBox.tsx
+++ packages/components/src/primitives/Box/BaseBox.tsx
@@ -5,7 +5,7 @@ export interface BaseBoxProps
   extends React.HTMLAttributes<HTMLElement>,
     CSSProp {
   /** Changes which HTML tag a component renders. */
-  as: keyof JSX.IntrinsicElements;
+  as?: keyof JSX.IntrinsicElements;
   /** The contents of the container. Any valid HTML. */
   children?: React.ReactNode;
   /** Boolean to set the aria-hidden prop of the container. */
