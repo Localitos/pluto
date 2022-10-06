aeb50730641de651bb30de3df64a08a854b61495
Rich Bachman <rb@richbachman.com>
2022-10-06 08:57:40 -0600

    chore(theme): export createGlobalStyle from xStyled (#77)

---
+++ .changeset/plenty-tables-appear.md
@@ -0,0 +1,5 @@
+---
+"@localyze-pluto/theme": patch
+---
+
+Export `createGlobalStyle` from xStyled so it can be used in other applications.
--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,4 +1,8 @@
-export { ThemeProvider, Preflight } from "@xstyled/styled-components";
+export {
+  createGlobalStyle,
+  Preflight,
+  ThemeProvider,
+} from "@xstyled/styled-components";
 export * from "./theme/default";
 export * from "./styles/base";
 export * from "./styles/global";
