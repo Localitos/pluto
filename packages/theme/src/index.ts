00756eb8624f331701e61ca9904e3b9cf55b65ea
Rich Bachman <rb@richbachman.com>
2022-10-07 06:52:15 -0600

    fix(theme): added types to export and correct peerDeps (#79)

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,8 +1,22 @@
-export {
-  createGlobalStyle,
-  Preflight,
-  ThemeProvider,
-} from "@xstyled/styled-components";
+import "styled-components";
+import "@xstyled/system";
+import { theme } from "./theme/default";
+
+type AppTheme = typeof theme;
+
+declare module "@xstyled/system" {
+  // XStyled wants an interace here.
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface Theme extends AppTheme {}
+}
+
+declare module "styled-components" {
+  // XStyled wants an interace here.
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends AppTheme {}
+}
+
+export * from "@xstyled/styled-components";
 export * from "./theme/default";
 export * from "./styles/base";
 export * from "./styles/global";
