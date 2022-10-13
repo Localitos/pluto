9d9eee659cb41185d51dd975b763fb1c11649485
richbachman <rb@richbachman.com>
2022-10-13 10:08:06 -0500

    feat(components): add button component

---
+++ .changeset/lucky-cows-lay.md
@@ -0,0 +1,5 @@
+---
+"@localyze-pluto/components": minor
+---
+
+[Button]: Added the Button component with one variant, primary. Button also has two sizes (small and large).
---
+++ packages/components/src/components/Button/Button.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import type { SystemProp, Theme } from "@xstyled/styled-components";
+import { Box } from "../../primitives/Box";
+
+type ButtonSizeOptions = "large" | "small";
+type ButtonVariantOptions = "primary";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /** Sets the render element of the component. Either 'a' or 'button'.*/
+  as?: "a" | "button";
+  /** Add a description comment for each prop. */
+  children?: NonNullable<React.ReactNode>;
+  /** If used as an 'a', the href is url that the link point to. */
+  href?: string;
+  /** Sets the button to be disabled. */
+  isDisabled?: boolean;
+  /** Sets the button state to loading. */
+  loading?: boolean;
+  /** Used with React Router or NextJS to set the route the anchor links to. */
+  to?: string;
+  /** Sets the size of the button. */
+  size?: ButtonSizeOptions;
+  /** Sets the variant of the button. */
+  variant: ButtonVariantOptions;
+}
+
+const getButtonVariantStyles = (
+  // Will be needed in the future.
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  variant: ButtonVariantOptions
+): {
+  backgroundColor: SystemProp<keyof Theme["colors"], Theme>;
+  borderColor: SystemProp<keyof Theme["colors"], Theme>;
+} => {
+  return {
+    backgroundColor: {
+      _: "colorBackgroundPrimary",
+      hover: "colorBackgroundPrimaryStrong",
+      disabled: "colorIconWeak",
+    },
+    borderColor: {
+      _: "colorBorderPrimary",
+      hover: "colorBorderPrimary",
+      disabled: "colorIconWeak",
+    },
+  };
+};
+
+/** A button is a clickable element which communicates that users can trigger an action. */
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      isDisabled,
+      loading,
+      size = "small",
+      variant = "primary",
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <Box.button
+        appearance="none"
+        aria-busy={loading ? "true" : "false"}
+        background="none"
+        borderRadius="borderRadius20"
+        borderStyle="borderSolid"
+        borderWidth="borderWidth10"
+        color="colorTextInverse"
+        cursor={{
+          _: "default",
+          hover: "pointer",
+          disabled: "not-allowed",
+          loading: "wait",
+        }}
+        disabled={isDisabled}
+        display="inline-block"
+        fontFamily="fontFamilyModerat"
+        fontSize={size === "large" ? "fontSize30" : "fontSize10"}
+        fontWeight="fontWeightMedium"
+        lineHeight={size === "large" ? "lineHeight40" : "lineHeight10"}
+        outlineColor={{ focus: "colorBorderPrimary" }}
+        outlineOffset={{ focus: "borderWidth20" }}
+        outlineStyle={{ focus: "solid" }}
+        outlineWidth={{ focus: "borderWidth20" }}
+        paddingBottom={size === "large" ? "space40" : "space30"}
+        paddingLeft={size === "large" ? "space50" : "space40"}
+        paddingRight={size === "large" ? "space50" : "space40"}
+        paddingTop={size === "large" ? "space40" : "space30"}
+        ref={ref}
+        textDecoration="none"
+        transition="background-color 100ms ease-in, border-color 100ms ease-in"
+        {...getButtonVariantStyles(variant)}
+        {...props}
+      >
+        {children}
+      </Box.button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export { Button };
