96ed29d676e5813cad5374ede6e8c9e88b49fd7f
Daniela Araújo <daniela@localyzeapp.com>
2022-10-28 13:52:06 +0200

    feat(components): add button text variant

--- packages/components/src/components/Button/Button.tsx
+++ packages/components/src/components/Button/Button.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import * as HeroOutlineIcons from "@heroicons/react/24/outline";
 import type { SystemProp, Theme } from "@xstyled/styled-components";
+import { styled, theme } from "@localyze-pluto/theme";
 import { Box } from "../../primitives/Box";
 import { Icon } from "../Icon";
 
 type ButtonSizeOptions = "large" | "small";
-type ButtonVariantOptions = "primary";
+type ButtonVariantOptions = "primary" | "text";
 
 type IconNames = keyof typeof HeroOutlineIcons;
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  extends Omit<React.HTMLAttributes<HTMLButtonElement>, "color"> {
   /** Sets the render element of the component. Either 'a' or 'button'.*/
   as?: "a" | "button";
   /** Add a description comment for each prop. */
@@ -34,14 +35,30 @@ export interface ButtonProps
 }
 
 const getButtonVariantStyles = (
-  // Will be needed in the future.
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   variant: ButtonVariantOptions
 ): {
-  backgroundColor: SystemProp<keyof Theme["colors"], Theme>;
-  borderColor: SystemProp<keyof Theme["colors"], Theme>;
+  backgroundColor?: SystemProp<keyof Theme["colors"], Theme>;
+  borderColor?: SystemProp<keyof Theme["colors"], Theme>;
+  borderWidth?: SystemProp<keyof Theme["borderWidths"], Theme>;
+  outlineColor?: SystemProp<keyof Theme["colors"], Theme>;
+  outlineOffset?: SystemProp<keyof Theme["borderWidths"], Theme>;
+  outlineStyle?: SystemProp<keyof Theme["borderStyles"], Theme>;
+  outlineWidth?: SystemProp<keyof Theme["borderWidths"], Theme>;
 } => {
+  if (variant === "text") {
+    return {
+      borderWidth: "borderWidth0",
+      outlineWidth: { focus: "borderWidth0" },
+      outlineOffset: { focus: "borderWidth0" },
+    };
+  }
+
   return {
+    borderWidth: "borderWidth10",
+    outlineColor: { focus: "colorBorderPrimary" },
+    outlineOffset: { focus: "borderWidth20" },
+    outlineStyle: { focus: "borderSolid" },
+    outlineWidth: { focus: "borderWidth20" },
     backgroundColor: {
       _: "colorBackgroundPrimary",
       hover: "colorBackgroundPrimaryStrong",
@@ -62,6 +79,33 @@ const getIcon = (iconName: IconNames, size: ButtonSizeOptions) => {
   return <Icon decorative icon={iconName} size={iconProps(size)} />;
 };
 
+const getTextColor = (
+  variant: ButtonVariantOptions
+): Record<string, string> => {
+  if (variant === "text") {
+    return {
+      active: theme.colors.colorTextLink,
+      hover: theme.colors.colorTextLinkStrong,
+      disabled: theme.colors.colorText,
+    };
+  }
+
+  return {
+    active: theme.colors.colorTextInverse,
+  };
+};
+
+const InnerButton = styled(Box.button)`
+  color: ${({ variant }: ButtonProps) => getTextColor(variant).active};
+
+  &::hover {
+    color: ${({ variant }: ButtonProps) => getTextColor(variant).hover};
+  }
+  &::disabled {
+    color: ${({ variant }: ButtonProps) => getTextColor(variant).disabled};
+  }
+`;
+
 /** A button is a clickable element which communicates that users can trigger an action. */
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   (
@@ -78,15 +122,13 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ref
   ) => {
     return (
-      <Box.button
+      <InnerButton
         alignItems="center"
         appearance="none"
         aria-busy={loading ? "true" : "false"}
         background="none"
         borderRadius="borderRadius20"
         borderStyle="borderSolid"
-        borderWidth="borderWidth10"
-        color="colorTextInverse"
         cursor={{
           _: "default",
           hover: "pointer",
@@ -100,10 +142,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         fontWeight="fontWeightMedium"
         gap="space30"
         lineHeight={size === "large" ? "lineHeight40" : "lineHeight30"}
-        outlineColor={{ focus: "colorBorderPrimary" }}
-        outlineOffset={{ focus: "borderWidth20" }}
-        outlineStyle={{ focus: "solid" }}
-        outlineWidth={{ focus: "borderWidth20" }}
         paddingBottom={size === "large" ? "space40" : "space30"}
         paddingLeft={size === "large" ? "space50" : "space40"}
         paddingRight={size === "large" ? "space50" : "space40"}
@@ -111,13 +149,14 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         ref={ref}
         textDecoration="none"
         transition="background-color 100ms ease-in, border-color 100ms ease-in"
+        variant={variant}
         {...getButtonVariantStyles(variant)}
         {...props}
       >
         {leadingIcon && getIcon(leadingIcon, size)}
         {children}
         {trailingIcon && getIcon(trailingIcon, size)}
-      </Box.button>
+      </InnerButton>
     );
   }
 );
