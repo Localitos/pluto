bf7623826302f1b219bcbe2e51dc897d6cdcf4eb
richbachman <rb@richbachman.com>
2022-10-28 14:17:17 -0500

    chore(components): remove useless test

--- packages/design-tokens/src/tokens/__tests__/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "../../../dist/js/variables";
-
-describe("Design tokens", () => {
-  it("should render js variables", () => {
-    const jsTokens = styles;
-    expect(jsTokens).toMatchSnapshot();
-  });
-});
