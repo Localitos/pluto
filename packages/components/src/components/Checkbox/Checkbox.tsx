9fcadb0e04e5a7583f23d7cca75a59bd162a0cae
Alinur <alinur.amangazy@nu.edu.kz>
2022-11-15 14:42:23 +0600

    fix: adjust checkbox placement with long labels

--- packages/components/src/components/Checkbox/Checkbox.tsx
+++ packages/components/src/components/Checkbox/Checkbox.tsx
@@ -84,6 +84,7 @@ const StyledCheckbox = styled.input`
   border-radius: 4px;
   margin-right: ${theme.space.space30};
   padding: 4px 0 0;
+  margin-top: 2px;
 
   &:focus {
     box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05), 0 0 0 2px ${
@@ -145,7 +146,7 @@ export const Checkbox = React.forwardRef<HTMLButtonElement, CheckboxProps>(
   ) => {
     return (
       <Box.div
-        alignItems="center"
+        alignItems="start"
         borderRadius="borderRadius30"
         display="flex"
         flexDirection="row"
