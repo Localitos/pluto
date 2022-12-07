909fadfdf92ae4d3fa6d201269b8fa1d726bea5c
Aaron Blum <aaron@localyzeapp.com>
2022-12-07 20:51:05 +0100

    fix(components): refactor dropzone to remove isDragReject

--- packages/components/src/components/Dropzone/getFileRestrictionText.test.ts
+++ packages/components/src/components/Dropzone/getFileRestrictionText.test.ts
@@ -3,13 +3,13 @@ import { getFileRestrictionText } from "./getFileRestrictionText";
 describe("getFileRestrictionText", () => {
   it("returns file size text if passed as parameter", () => {
     expect(getFileRestrictionText({}, 1, 3 * 1024 * 1024)).toBe(
-      "File must be no larger than 3MB, and you can upload only 1 file."
+      "You can upload 1 file. File must be no larger than 3MB."
     );
   });
 
   it("returns file type text if passed as parameter", () => {
     expect(getFileRestrictionText({ "image/jpeg": [".jpg"] }, 1)).toBe(
-      "File must be JPG format, and you can upload only 1 file."
+      "You can upload 1 file. File must be JPG format"
     );
 
     expect(
@@ -20,16 +20,14 @@ describe("getFileRestrictionText", () => {
         },
         1
       )
-    ).toBe(
-      "File must be JPG, JPEG, PDF format, and you can upload only 1 file."
-    );
+    ).toBe("You can upload 1 file. File must be JPG, JPEG, PDF format");
   });
 
   it("returns file type and size text if passed as parameter", () => {
     expect(
       getFileRestrictionText({ "image/jpeg": [".jpg"] }, 1, 3 * 1024 * 1024)
     ).toBe(
-      "File must be JPG format, no larger than 3MB, and you can upload only 1 file."
+      "You can upload 1 file. File must be JPG format, no larger than 3MB."
     );
   });
 });
