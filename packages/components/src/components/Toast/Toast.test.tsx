e1379044dcdc74945de180f436b927e729053af5
Rich Bachman <rb@richbachman.com>
2022-12-21 10:14:02 -0700

    feat(components): added useToast hook and ToastContainer (#360)

--- packages/components/src/components/Toast/Toast.test.tsx
+++ packages/components/src/components/Toast/Toast.test.tsx
@@ -1,7 +1,23 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
+import { Button } from "../Button";
 import { Default } from "./Toast.stories";
+import { useToast } from "./useToast";
+import { ToastContainer } from "./ToastContainer";
+
+const SampleApp = (): JSX.Element => {
+  const toast = useToast();
+
+  return (
+    <Button
+      onClick={() => toast("Toast message!", "success")}
+      variant="primary"
+    >
+      Open Toast
+    </Button>
+  );
+};
 
 describe("<Toast />", () => {
   it("should open and close a toast", async () => {
@@ -21,5 +37,22 @@ describe("<Toast />", () => {
     await waitFor(() => expect(renderedToast).not.toBeInTheDocument(), {
       timeout: 1000,
     });
+  });
+});
+
+describe("<ToastContainer />", () => {
+  it("should open a toast in the ToastContainer", async () => {
+    render(
+      <ToastContainer>
+        <SampleApp />
+      </ToastContainer>
+    );
+
+    const renderedOpenButton = screen.getByRole("button");
+    await userEvent.click(renderedOpenButton);
+
+    const renderedToast = screen.getByText("Toast message!");
+    expect(screen.getByRole("region")).toBeInTheDocument();
+    expect(renderedToast).toBeInTheDocument();
   });
 });
