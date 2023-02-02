7afc2cd63a372ce5e1eff3f772f9c112ad9f90d0
richbachman <rb@richbachman.com>
2023-02-02 10:30:54 -0600

    feat(components): add ControlledFormSelect for react hook form

---
+++ packages/components/src/components/FormSelect/ControlledFormSelect.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Controller } from "react-hook-form";
+import type { ControllerProps, Control } from "react-hook-form";
+import { FormSelect } from "./FormSelect";
+import type { FormSelectProps } from "./FormSelect";
+
+export interface ControlledFormSelectProps
+  extends Omit<ControllerProps, "control" | "defaultValue" | "render">,
+    Omit<FormSelectProps, "defaultValue" | "name" | "value"> {
+  /** Invoked with `useForm`. Set to any to allow `any` number of form inputs. */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  control?: Control<any>;
+}
+
+const ControlledFormSelect = ({
+  control,
+  disabled,
+  helpText,
+  id,
+  items,
+  label,
+  name,
+  placeholder,
+  required,
+  ...props
+}: ControlledFormSelectProps): JSX.Element => {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState }) => (
+        <FormSelect
+          {...field}
+          disabled={disabled}
+          hasError={fieldState.invalid}
+          helpText={helpText}
+          id={id}
+          items={items}
+          label={label}
+          placeholder={placeholder}
+          required={(props.rules?.required as boolean) || required}
+          setValue={field.onChange}
+          {...props}
+        />
+      )}
+      rules={props.rules}
+    />
+  );
+};
+
+ControlledFormSelect.displayName = "ControlledFormSelect";
+
+ControlledFormSelect.propTypes = {
+  control: PropTypes.any,
+  disabled: PropTypes.bool,
+  helpText: PropTypes.node,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  required: PropTypes.bool,
+};
+
+export { ControlledFormSelect };
--- packages/components/src/components/FormSelect/FormSelect.test.tsx
+++ packages/components/src/components/FormSelect/FormSelect.test.tsx
@@ -1,10 +1,58 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
+import { useForm } from "react-hook-form";
+import userEvent from "@testing-library/user-event";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { Box } from "../../primitives/Box";
+import type { SelectProps } from "../Select";
 import {
   Default as FormSelect,
   Required as RequiredFormSelect,
   Disabled as DisabledFormSelect,
 } from "./FormSelect.stories";
+import { ControlledFormSelect } from "./ControlledFormSelect";
+
+const selectItems: SelectProps["items"] = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "vanilla", label: "Vanilla" },
+];
+
+const ReactHookFormExample = (): JSX.Element => {
+  const schema = yup.object().shape({
+    flavor: yup.string().required("A flavor is required."),
+  });
+
+  interface FormInputs {
+    flavor: string;
+  }
+
+  const { control, formState } = useForm<FormInputs>({
+    defaultValues: {
+      flavor: "",
+    },
+    resolver: yupResolver(schema),
+  });
+  return (
+    <Box.form>
+      <ControlledFormSelect
+        control={control}
+        data-testid="test"
+        hasError={!!formState.errors.flavor}
+        helpText={
+          formState.errors.flavor
+            ? formState.errors.flavor.message
+            : "Please select a flavor."
+        }
+        id="flavor"
+        items={selectItems}
+        label="Select a flavor"
+        name="flavor"
+        required
+      />
+    </Box.form>
+  );
+};
 
 describe("<FormSelect />", () => {
   it("renders correctly", () => {
@@ -29,5 +77,23 @@ describe("<FormSelect />", () => {
     expect(
       screen.getByText("Please choose one of the values.")
     ).toBeInTheDocument();
+  });
+
+  it("should render a ControlledFormSelect", async () => {
+    render(<ReactHookFormExample />);
+    const controlledSelect = screen.getByRole("combobox");
+    const selectOptions = screen.getAllByRole("option", { hidden: true });
+
+    expect(controlledSelect).toBeInTheDocument();
+    expect(screen.getByLabelText("Select a flavor")).toBeInTheDocument();
+    expect(screen.getByText("Please select a flavor.")).toBeInTheDocument();
+    expect(controlledSelect).toBeRequired();
+    expect(controlledSelect).toHaveAttribute("id", "flavor");
+    expect(controlledSelect).toHaveAttribute("data-testid", "test");
+
+    await userEvent.click(controlledSelect);
+    await userEvent.click(selectOptions[2]);
+
+    expect(selectOptions[2]).toHaveAttribute("aria-selected", "true");
   });
 });
