b96bbf0db23114cf86858815f8b09a3f57748687
Rafael Specht <rafaelspdasilva@gmail.com>
2023-03-24 11:05:35 -0300

    fix: use Box.div on ProgressBar fixing tokens usage

--- packages/components/src/components/ProgressBar/ProgressBar.tsx
+++ packages/components/src/components/ProgressBar/ProgressBar.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { styled, theme } from "@localyze-pluto/theme";
-import { Root, Indicator, ProgressProps } from "@radix-ui/react-progress";
+import { Root, Indicator } from "@radix-ui/react-progress";
 import { SystemProp, Theme } from "@xstyled/styled-components";
-import get from "lodash/get";
+import { Box } from "../../primitives/Box";
 
 type ProgressBarSizeOptions = "large" | "small";
 
@@ -17,47 +16,6 @@ export interface ProgressBarProps {
   size?: ProgressBarSizeOptions;
 }
 
-interface StyledProgressProps extends Omit<ProgressProps, "value"> {
-  value: number;
-  background?: SystemProp<keyof Theme["colors"], Theme>;
-  h: SystemProp<keyof Theme["space"], Theme>;
-  radius: SystemProp<keyof Theme["radii"], Theme>;
-}
-
-interface StyledIndicatorProps {
-  background?: SystemProp<keyof Theme["colors"], Theme>;
-  radius: SystemProp<keyof Theme["radii"], Theme>;
-}
-
-const StyledProgress = styled.div`
-  background-color: ${(props: StyledProgressProps) =>
-    get(
-      theme.colors,
-      String(props.background),
-      theme.colors.colorBackgroundWeak
-    )};
-  border-radius: ${(props: StyledProgressProps) =>
-    get(theme.radii, String(props.radius), "")};
-  height: ${(props: StyledProgressProps) =>
-    get(theme.space, String(props.h), "")};
-  overflow: hidden;
-  position: relative;
-`;
-
-const StyledIndicator = styled.div`
-  height: 100%;
-  width: 100%;
-  transition: transform 660ms cubic-bezier(0.65, 0, 0.35, 1);
-  border-radius: ${(props: StyledIndicatorProps) =>
-    get(theme.radii, String(props.radius), "")};
-  background-color: ${(props: StyledIndicatorProps) =>
-    get(
-      theme.colors,
-      String(props.background),
-      theme.colors.colorBackgroundPrimaryWeak
-    )};
-`;
-
 /** Displays an indicator showing the completion progress of a task, typically displayed as a progress bar. */
 const ProgressBar = React.forwardRef<HTMLDivElement, ProgressBarProps>(
   (
@@ -67,22 +25,27 @@ const ProgressBar = React.forwardRef<HTMLDivElement, ProgressBarProps>(
     const borderRadius = size === "large" ? "borderRadius50" : "borderRadius10";
 
     return (
-      <StyledProgress
+      <Box.div
         as={Root}
-        background={backgroundColor}
-        h={size === "large" ? "space40" : "space25"}
-        radius={borderRadius}
+        backgroundColor={backgroundColor || "colorBackgroundWeak"}
+        borderRadius={borderRadius}
+        h={size === "large" ? "0.75rem" : "0.4rem"}
+        overflow="hidden"
+        position="relative"
         ref={ref}
         value={value}
         {...props}
       >
-        <StyledIndicator
+        <Box.div
           as={Indicator}
-          background={indicatorColor}
-          radius={borderRadius}
+          backgroundColor={indicatorColor || "colorBackgroundPrimaryWeak"}
+          borderRadius={borderRadius}
+          h="100%"
           style={{ transform: `translateX(-${100 - value}%)` }}
+          transition="transform 660ms cubic-bezier(0.65, 0, 0.35, 1)"
+          w="100%"
         />
-      </StyledProgress>
+      </Box.div>
     );
   }
 );
