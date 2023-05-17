c05ec830670e4333a211d445fe8a889a7f4f501f
Aaron Blum <aaron@localyzeapp.com>
2023-05-17 16:00:53 +0200

    feat(components): add test for usePagination

--- packages/components/src/components/Pagination/usePagination.ts
+++ packages/components/src/components/Pagination/usePagination.ts
@@ -2,14 +2,17 @@ import { useMemo } from "react";
 
 const DOTS = "...";
 
-const range = (start: number, end: number): string[] => {
+/* This function is used to generate the range of pages that 
+ are visible for either the left side, right side, or middle pages of the pagination  */
+const range = (start: number, end: number): number[] => {
   const length = end - start + 1;
-  return Array.from({ length }, (_, idx) => `${idx + start}`);
+  return Array.from({ length }, (_, idx) => Number(`${idx + start}`));
 };
 
+/* This hook generates an array that calculates where to put the dots depending on the current page and the number of pages */
 export const usePagination = (
-  totalCount: number,
-  pagesAroundCurrentPage = 1, // The number of pages to display to each side of the current page
+  pageCount: number,
+  pagesAroundCurrentPage: number, // The number of pages to display to each side of the current page
   currentPage: number
 ): (number | string)[] => {
   return useMemo(() => {
@@ -17,7 +20,7 @@ export const usePagination = (
     const fixedItems = pagesAroundCurrentPage + 5;
     const fixedItemsOnEachSide = 3;
     const firstPage = 1;
-    const lastPage = totalCount;
+    const lastPage = pageCount;
 
     /*
       If the number of fixed items is less than or equal to the total count we want to show in our
@@ -38,9 +41,9 @@ export const usePagination = (
 
     const shouldShowLeftDots = leftSiblingIndex >= 5;
     const shouldShowRightDots =
-      rightSiblingIndex < totalCount - fixedItemsOnEachSide;
+      rightSiblingIndex < pageCount - fixedItemsOnEachSide;
 
-    const lastPageIndex = totalCount;
+    const lastPageIndex = pageCount;
 
     const ifOnFirstFivePages = !shouldShowLeftDots && shouldShowRightDots;
 
@@ -48,7 +51,7 @@ export const usePagination = (
       const leftItemCount = fixedItemsOnEachSide + pagesAroundCurrentPage + 1;
       const leftRange = range(firstPage, leftItemCount);
 
-      return [...leftRange, DOTS, totalCount];
+      return [...leftRange, DOTS, pageCount];
     }
 
     const ifOnLastFivePages = shouldShowLeftDots && !shouldShowRightDots;
@@ -65,5 +68,5 @@ export const usePagination = (
     }
 
     return [];
-  }, [totalCount, pagesAroundCurrentPage, currentPage]);
+  }, [pageCount, pagesAroundCurrentPage, currentPage]);
 };
