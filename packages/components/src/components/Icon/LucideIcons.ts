eb000af6d90ee1e43f477c0c73e2007b2d59dab9
Rafael Specht da Silva <rafaelspdasilva@gmail.com>
2023-06-06 15:59:20 -0300

    feat: add building-2, pie-chart, puzzle and wallet as available icons (#850)

--- packages/components/src/components/Icon/LucideIcons.ts
+++ packages/components/src/components/Icon/LucideIcons.ts
@@ -1,15 +1,23 @@
 import {
   BaggageClaim,
+  Building2,
   HeartHandshake,
   Palette,
+  PieChart,
+  Puzzle,
   Slack,
+  Wallet,
   Icon,
 } from "lucide-react";
 import { LucideIconName } from "./types/LucideIconName";
 
 export const LucideIcons: Record<LucideIconName, Icon> = {
   "baggage-claim": BaggageClaim,
+  "building-2": Building2,
   "heart-handshake": HeartHandshake,
   palette: Palette,
   slack: Slack,
+  "pie-chart": PieChart,
+  puzzle: Puzzle,
+  wallet: Wallet,
 };
