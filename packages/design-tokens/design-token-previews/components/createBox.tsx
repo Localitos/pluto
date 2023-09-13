e1db6b11cebd75bc2a24cb841a2860b22f8155d0
Aaron Blum <aaron@localyzeapp.com>
2023-09-13 17:16:24 +0200

    fix(design-tokens): key issue in TokensTable

--- packages/design-tokens/design-token-previews/components/createBox.tsx
+++ packages/design-tokens/design-token-previews/components/createBox.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import includes from "lodash/includes";
+import replace from "lodash/replace";
 import { TokenEntry } from "../types/TokenEntry";
 import { Box } from "../../../components/src/primitives/Box";
 
@@ -6,9 +8,13 @@ export const createBox =
   (tokenAttribute: string, defaultProps = {}, text = "\0") =>
   // eslint-disable-next-line react/display-name
   ([, token]: TokenEntry): JSX.Element => {
+    let tokenValue = token.value;
+    if (includes(token.value, "-")) {
+      tokenValue = replace(token.value, "-", "");
+    }
     const props = {
       ...defaultProps,
-      [tokenAttribute]: token.value,
+      [tokenAttribute]: tokenValue,
     };
     return <Box.div {...props}>{text}</Box.div>;
   };
