44d397b4d02561d2ff8176cc121823c87dab72a7
Aaron Blum <aaron@localyzeapp.com>
2023-10-26 09:15:46 +0200

    fix(components): added test for onclick to menu

--- packages/components/src/components/Menu/Menu.test.tsx
+++ packages/components/src/components/Menu/Menu.test.tsx
@@ -38,9 +38,8 @@ describe("<Menu />", () => {
   });
 
   it("allows a disabled prop for a menu item and disables the button", async () => {
-    render(
-      <Menu items={[{ onClick: jest.fn(), label: "Item 1", disabled: true }]} />
-    );
+    const onClick = jest.fn();
+    render(<Menu items={[{ onClick, label: "Item 1", disabled: true }]} />);
 
     const menuButton = screen.getByRole("button");
     expect(menuButton).toBeInTheDocument();
@@ -50,5 +49,22 @@ describe("<Menu />", () => {
     expect(screen.getByRole("presentation")).toBeInTheDocument();
 
     expect(screen.getByRole("button", { name: /item 1/i })).toBeDisabled();
+    await userEvent.click(screen.getByRole("button", { name: /item 1/i }));
+    expect(onClick).not.toHaveBeenCalled();
+  });
+
+  it("calls onClick", async () => {
+    const onClick = jest.fn();
+    render(<Menu items={[{ onClick, label: "Item 1" }]} />);
+
+    const menuButton = screen.getByRole("button");
+    expect(menuButton).toBeInTheDocument();
+
+    await userEvent.click(menuButton);
+
+    expect(screen.getByRole("presentation")).toBeInTheDocument();
+
+    await userEvent.click(screen.getByRole("button", { name: /item 1/i }));
+    expect(onClick).toHaveBeenCalled();
   });
 });
