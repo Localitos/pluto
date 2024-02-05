c3a07600bc89a1619f748d5f506e1913aa46b607
Ana Paula Vale <apvalefin@gmail.com>
2024-02-05 16:31:37 +0000

    feat: accepts a children prop

--- .changeset/curly-suits-cry.md
+++
@@ -1,8 +0,0 @@
----
-"@localyze-pluto/components": patch
----
-
-[ContentCard]:
-
-- Add `buttonAsLink` prop to make the button act as a link and open in a new tab
-  when clicked
--- packages/components/src/components/ContentCard/ContentCard.stories.tsx
+++ packages/components/src/components/ContentCard/ContentCard.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryFn } from "@storybook/react";
 import React from "react";
 import { Box } from "../../primitives/Box";
+import { Button } from "../Button";
 import { ContentCard } from "./ContentCard";
 
 export default {
@@ -19,7 +20,6 @@ const defaultProps = {
   text: "Lorem ipsum dolor sit, consectetur. Aliquam ac a. Ligula at et, sodales vel purus.",
   date: "Mon, Sept 5 2022",
   tag: "🏠 Permanent Housing",
-  buttonText: "Go to Page",
   imageSrc: "/images/beach-seal-rocks.jpg",
   imageAlt: "a beach",
 };
@@ -31,11 +31,21 @@ Default.args = {
   linkHref,
 };
 
-export const ButtonAsLink = Template.bind({});
-ButtonAsLink.args = {
+export const WithButton = Template.bind({});
+WithButton.args = {
   ...defaultProps,
-  linkHref,
-  buttonAsLink: true,
+  children: (
+    <Button
+      as="a"
+      fullWidth
+      href={linkHref}
+      rel="noopener noreferrer"
+      target="_blank"
+      variant="secondary"
+    >
+      Go to Page
+    </Button>
+  ),
 };
 
 export const Interactive = Template.bind({});
