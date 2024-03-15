b934f95553d3339a57be41a0bed89324d46a0919
Helder Ferreira <helderferreira@Helders-MBP.home>
2024-03-15 16:16:50 +0000

    fix: fix getTokenName

--- packages/design-tokens/docs/utils/getTokenName.tsx
+++ packages/design-tokens/docs/utils/getTokenName.tsx
@@ -1,14 +1,14 @@
 import camelCase from "lodash/camelCase";
 import capitalize from "lodash/capitalize";
-import { TokenEntry } from "../types/TokenEntry";
 import { Token } from "../types/Token";
+import { TokenTuple } from "../types/TokenTuple";
 import { getTokenKey } from "./getTokenKey";
 
 export const getTokenName = (
   tokens: Record<string, Record<string, Token>>,
-): (([suffix]: TokenEntry) => string) => {
+): (([, suffix]: TokenTuple) => string) => {
   const prefix = getTokenKey(tokens);
-  return ([suffix]: TokenEntry): string => {
-    return camelCase(`${prefix}${capitalize(suffix)}`);
+  return ([, suffix]: TokenTuple): string => {
+    return camelCase(`${prefix}${capitalize(suffix[0])}`);
   };
 };
