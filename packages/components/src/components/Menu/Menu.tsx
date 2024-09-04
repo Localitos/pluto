82668b3f505073f3bda7af3ae6a80b5d1232e741
Stephan Leibelt <stephan@localyzeapp.com>
2024-09-04 15:19:54 +0200

    fix(components): Update menu outline color to gray

--- packages/components/src/components/Menu/Menu.tsx
+++ packages/components/src/components/Menu/Menu.tsx
@@ -63,7 +63,7 @@ const Menu = React.forwardRef<HTMLButtonElement, MenuProps>(
             flexDirection="column"
             maxWidth="280px"
             minWidth="160px"
-            outlineColor="colorBorderPrimary"
+            outlineColor="colorBorderweaker"
             outlineStyle="borderStyleSolid"
             outlineWidth="borderWidth10"
           >
