1c3d01792e0d3a35b3015cf9365f0170e7b26281
Rafael Specht <rafaelspdasilva@gmail.com>
2024-09-18 14:40:30 -0300

    using category instead name to the prop, wrapping table with a div to avoid 100% height

--- packages/design-tokens/docs/components/TokensTable.tsx
+++ packages/design-tokens/docs/components/TokensTable.tsx
@@ -13,22 +13,23 @@ import {
 import { Token } from "../types/Token";
 import { buildTokensTableRows } from "../utils";
 import { TokenRow } from "../types/TokenRow";
+import { Box } from "../../../components/src/primitives/Box";
 import { TOKEN_COLUMNS } from "./constants";
 import { TokenName } from "./TokenName";
 
 export const TokensTable = ({
   data,
-  name,
+  category,
 }: {
   data: Record<string, Record<string, Token>>;
-  name: string;
+  category: string;
 }): JSX.Element => {
   const tokenNames: Array<string> = filter(
     keys(data),
     (item) => item !== "default",
   );
   const columnGroups = map(tokenNames, (token) => {
-    return TOKEN_COLUMNS[name][token];
+    return TOKEN_COLUMNS[category][token];
   });
   const rowGroups: TokenRow[][] = map(columnGroups, (columns, index) => {
     const token = tokenNames[index];
@@ -36,41 +37,43 @@ export const TokensTable = ({
   });
 
   return (
-    <Table style={{ width: "100%" }}>
-      <THead>
-        <Tr>
-          {map(columnGroups[0], (column) => {
-            return (
-              <Th key={column.name}>
-                <h3>{column.name}</h3>
-              </Th>
-            );
-          })}
-        </Tr>
-      </THead>
-      <TBody>
-        {map(rowGroups, (rows) =>
-          map(rows, (row) => {
-            const tokenName = row[0] as string;
-            return (
-              <Tr key={tokenName}>
-                {map(row, (cell, index) => {
-                  const column = columnGroups[0][index]?.name;
-                  return (
-                    <Td key={`${tokenName}${column}`}>
-                      {index === 0 ? (
-                        <TokenName tokenName={cell as string} />
-                      ) : (
-                        cell
-                      )}
-                    </Td>
-                  );
-                })}
-              </Tr>
-            );
-          }),
-        )}
-      </TBody>
-    </Table>
+    <Box.div>
+      <Table style={{ width: "100%" }}>
+        <THead>
+          <Tr>
+            {map(columnGroups[0], (column) => {
+              return (
+                <Th key={column.name}>
+                  <h3>{column.name}</h3>
+                </Th>
+              );
+            })}
+          </Tr>
+        </THead>
+        <TBody>
+          {map(rowGroups, (rows) =>
+            map(rows, (row) => {
+              const tokenName = row[0] as string;
+              return (
+                <Tr key={tokenName}>
+                  {map(row, (cell, index) => {
+                    const column = columnGroups[0][index]?.name;
+                    return (
+                      <Td key={`${tokenName}${column}`}>
+                        {index === 0 ? (
+                          <TokenName tokenName={cell as string} />
+                        ) : (
+                          cell
+                        )}
+                      </Td>
+                    );
+                  })}
+                </Tr>
+              );
+            }),
+          )}
+        </TBody>
+      </Table>
+    </Box.div>
   );
 };
